4be353f28162aee18d16cff249554c8930bb279d
Nhat Tan <tanln01072001@gmail.com>
2024-03-03 21:42:03 +0700

    gen code

--- AutomobileLibrary/Repository/CarRepository.cs
+++ AutomobileLibrary/Repository/CarRepository.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AutomobileLibrary.BussinessObject;
 using AutomobileLibrary.DataAccess;
 namespace AutomobileLibrary.Repository
 {
     public class CarRepository : ICarRepository
     {//cung cấp các phương thức để thao tác với đối tượng Car trong cơ sở dữ liệu.
-        public Car GetCarByID(int carId) => CarDBContext.Instance.GetCarByID(carId);
-        public IEnumerable<Car> GetCars() => CarDBContext.Instance.GetCarList();
-        public void InsertCar(Car car) => CarDBContext.Instance.AddNew(car);
-        public void DeleteCar(int carId) => CarDBContext.Instance.Remove(carId);
-        public void UpdateCar(Car car) => CarDBContext.Instance.Update(car);
+        public Car GetCarByID(int carId) => CarDAO.Instance.GetCarByID(carId);
+        public IEnumerable<Car> GetCars() => CarDAO.Instance.GetCarList();
+        public void InsertCar(Car car) => CarDAO.Instance.AddNew(car);
+        public void DeleteCar(int carId) => CarDAO.Instance.Remove(carId);
+        public void UpdateCar(Car car) => CarDAO.Instance.Update(car);
 
     }
 }
--- AutomobileLibrary/Repository/ICarRepository.cs
+++ AutomobileLibrary/Repository/ICarRepository.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AutomobileLibrary.BussinessObject;
 using System.Collections;
+using AutomobileLibrary.DataAccess;
 namespace AutomobileLibrary.Repository
 {// kho lưu trữ của các đối tượng Car cần triển khai
     public interface ICarRepository
     {
         IEnumerable<Car> GetCars();
         Car GetCarByID(int carId);
-        void InsertCar (Car car);
-        void UpdateCar (Car car);
-        void DeleteCar (int carId);
+        void InsertCar(Car car);
+        void UpdateCar(Car car);
+        void DeleteCar(int carId);
     }
 }
