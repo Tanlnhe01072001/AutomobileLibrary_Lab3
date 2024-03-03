66253872fd11c2de3d0bf8d1b8d2b371e94c13e9
Nhat Tan <tanln01072001@gmail.com>
2024-03-03 20:52:12 +0700

    Add project files.

---
+++ AutomobileLibrary/Repository/ICarRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomobileLibrary.BussinessObject;
+using System.Collections;
+namespace AutomobileLibrary.Repository
+{// kho lưu trữ của các đối tượng Car cần triển khai
+    public interface ICarRepository
+    {
+        IEnumerable<Car> GetCars();
+        Car GetCarByID(int carId);
+        void InsertCar (Car car);
+        void UpdateCar (Car car);
+        void DeleteCar (int carId);
+    }
+}
